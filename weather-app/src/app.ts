a4f2a6f3ced23017ea87f694baeb53c71815a55b
christian29200 <lefatoum@gmail.com>
2022-06-03 00:20:24 +0200

    final

--- weather-app/src/app.ts
+++ weather-app/src/app.ts
@@ -1,21 +1,24 @@
 import { WeatherService } from './services/weather-service'
 import { inject } from 'aurelia-framework';
+import { List } from 'interfaces/weather';
 
 @inject(WeatherService)
 export class App {
+  city = 'New York'; // added default city
+  forcastlist: List; 
   constructor(private weatherService: WeatherService) {}
 
   activate() {
-    console.log('activate')
     this.forecast();
   }
 
   forecast() {
-    this.weatherService.getForecast('New York').then(forecast => {
-      console.log(forecast);
+    let city = this.city //get the city from user
+    this.weatherService.getForecast(city).then(forecast => {
+      this.forcastlist = forecast as List;
+      console.log(this.forcastlist);
     }).catch(error => {
       console.log(error);
     });
   }
-  public message: string = 'Hello World!';
 }
--- weather-app/src/services/weather-service.ts
+++ weather-app/src/services/weather-service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from 'aurelia-http-client';
 import { autoinject } from "aurelia-framework";
+import { WeatherResponce } from 'interfaces/weather';
+
 
 @autoinject
 export class WeatherService {
@@ -14,11 +16,13 @@ export class WeatherService {
     const appId = 'cfe13ffc0ed739646109916072a3e6bf';
     return new Promise((resolve, reject) => {
       this.httpClient.get('forecast?q=' + city + '&appid=' + appId).then(httpResponse=> {
-        var forecast = JSON.parse(httpResponse.response); 
-        resolve(forecast);
+        var forecast: WeatherResponce = JSON.parse(httpResponse.response);
+        resolve(forecast.list);
       }).catch( error => {
         reject(error);
       });
     });
   }
 }
+
+
